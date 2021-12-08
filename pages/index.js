6fdb8a4951068afa916c9e65ad05db9f397b2788
jaewoong_ryu <jaewoong_ryu@tmax.co.kr>
2021-12-08 18:46:42 +0900

    chore: test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div style="background-color: '#3367D6'">Hi im next!</div>;
+  return <div style="background-color: #3367D6">Hi im next!</div>;
 };
 
 export default Home;
