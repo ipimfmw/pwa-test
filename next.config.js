4e03f326e1ba49b39b6fd6bcd13f0f005c3792f7
ipimfmw <ipimfmw@nate.com>
2021-12-09 18:04:44 +0900

    chore: fix config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/ipimfmw/pwa-test/tree/gh-pages"
-    : "",
+  // assetPrefix: isProd
+  //   ? "https://cdn.statically.io/gh/ipimfmw/pwa-test/tree/gh-pages"
+  //   : "",
+  assetPrefix: "/pwa-test",
 };
