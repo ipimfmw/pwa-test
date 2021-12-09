a5452a458ef9283a7f866ee5ee0424a17533cf60
jaewoong_ryu <jaewoong_ryu@tmax.co.kr>
2021-12-09 10:40:29 +0900

    fix directory structure

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Head from "next/head";
+
 const Home = () => {
-  return <div>Hi im next!</div>;
+  return (
+    <div>
+      <Head>
+        <title>NFT-mobilefront</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="../images/89.png" />
+      </Head>
+      Hi im next!
+    </div>
+  );
 };
 
 export default Home;
