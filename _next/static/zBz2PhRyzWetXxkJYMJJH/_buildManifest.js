411f5b9b6db68409d8b6b0de802855ea16792e77
ipimfmw <ipimfmw@nate.com>
2021-12-10 00:15:16 +0900

    Deploy Next.js to gh-pages

--- _next/static/zBz2PhRyzWetXxkJYMJJH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-16107d9d2b9f47e2.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],"/_offline":["static/chunks/pages/_offline-d0e78813f2aea7ce.js"],sortedPages:["/","/_app","/_error","/_offline"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
