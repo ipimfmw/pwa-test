bede160bece6467712cc19c6c838855ea52861a3
ipimfmw <ipimfmw@nate.com>
2021-12-09 22:57:06 +0900

    chore: add sw about offline

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
         <title>NFT-mobilefront</title>
         <link rel="manifest" href="manifest.json" />
         <link rel="apple-touch-icon" href="image/89.png" />
+        <Script src="https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate" />
       </Head>
       Hi im next! and this is deploying test!22222
     </div>
